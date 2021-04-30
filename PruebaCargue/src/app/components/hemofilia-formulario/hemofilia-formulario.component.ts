2dc15dec90ebb7307178d3e8bcb92a00abba4101
Yicsson Mosquera <Yibsondavid@gmail.com>
2021-04-29 22:53:03 -0500

    Funciones de selección múltiple

--- PruebaCargue/src/app/components/hemofilia-formulario/hemofilia-formulario.component.ts
+++ PruebaCargue/src/app/components/hemofilia-formulario/hemofilia-formulario.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import {HemofiliaService} from '../../services/hemofilia/hemofilia.service'
+import {Hemofilia} from '../../models/hemofilia'
 @Component({
   selector: 'app-hemofilia-formulario',
   templateUrl: './hemofilia-formulario.component.html',
@@ -10,10 +12,350 @@ export class HemofiliaFormularioComponent implements OnInit {
   model: NgbDateStruct
   horas:any
   minutos:any
+  tipodocumento:any
+  Sexo:any
+  RegimenAF:any
+  CEtnica:any
+  GProblacional:any
+  EstagFechacorte:any
+  Programaconsejeria:any
+  MPDiagnostico:any
+  TPDiagnostico:any
+  CLSeveridad:any
+  ASHemofilia:any
+  FactorR:any
+  Esquema1:any
+  FactorrecibidoTA:any
+  EsquemaTA:any
+  Frecuenciasemana:any;
+  Modalidadtratamiento:any
+  Viaadministracion:any;
+  ProfesionalSP:any;
+  Hermatosis:any
+  PacienteITI:any;
+  RecibioITI:any;
+  Ingiborfechacorte:any;
+  hermofiliacronica:any;
+  VHC:any;
+  VHB:any;
+  VIH:any;
+  Pseudotumor:any;
+  Fractura:any;
+  Anasifilis:any;
+  Novedad:any;
+  Causademuerte:any;
 
-  constructor() { }
+  hemofili:Hemofilia ={
+    CAMPO_1:'',
+    CAMPO_2:'',
+    CAMPO_3:'',
+    CAMPO_4:'',
+    CAMPO_5:'',
+    CAMPO_6:'',
+    CAMPO_7:'',
+    CAMPO_8:'',
+    CAMPO_9:'',
+    CAMPO_10:'',
+    CAMPO_11:'',
+    CAMPO_12:'',
+    CAMPO_13:'',
+    CAMPO_14:'',
+    CAMPO_15:'',
+    CAMPO_16:'',
+    CAMPO_17:'',
+    CAMPO_18:'',
+    CAMPO_19:'',
+    CAMPO_20:'',
+    CAMPO_21:'',
+    CAMPO_22:'',
+    CAMPO_23:'',
+    CAMPO_24:'',
+    CAMPO_25:'',
+    CAMPO_26:'',
+    CAMPO_27:'',
+    CAMPO_28:'',
+    CAMPO_29:'',
+    CAMPO_30:'',
+    CAMPO_31:'',
+    CAMPO_32:'',
+    CAMPO_32_1:'',
+    CAMPO_32_2:'',
+    CAMPO_32_3:'',
+    CAMPO_32_4:'',
+    CAMPO_33:'',
+    CAMPO_34:'',
+    CAMPO_35:'',
+    CAMPO_36:'',
+    CAMPO_37:'',
+    CAMPO_38:'',
+    CAMPO_39:'',
+    CAMPO_40:'',
+    CAMPO_40_1:'',
+    CAMPO_40_2:'',
+    CAMPO_41:'',
+    CAMPO_42:'',
+    CAMPO_43:'',
+    CAMPO44:'',
+    CAMPO_45:'',
+    CAMPO_46:'',
+    CAMPO_47_1:'',
+    CAMPO_47_2:'',
+    CAMPO_47_3:'',
+    CAMPO_48:'',
+    CAMPO_48_1:'',
+    CAMPO_48_2:'',
+    CAMPO_48_3:'',
+    CAMPO_48_4:'',
+    CAMPO_49:'',
+    CAMPO_49_1:'',
+    CAMPO_50:'',
+    CAMPO_51:'',
+    CAMPO_52:'',
+    CAMPO_53:'',
+    CAMPO_54:'',
+    CAMPO_55:'',
+    CAMPO_55_1:'',
+    CAMPO_56:'',
+    CAMPO_56_1:'',
+    CAMPO_57:'',
+    CAMPO_57_1:'',
+    CAMPO_57_2:'',
+    CAMPO_57_3:'',
+    CAMPO_57_4:'',
+    CAMPO_57_5:'',
+    CAMPO_57_6:'',
+    CAMPO_57_7:'',
+    CAMPO_57_8:'',
+    CAMPO_57_9:'',
+    CAMPO_57_10:'',
+    CAMPO_57_11:'',
+    CAMPO_57_12:'',
+    CAMPO_57_13:'',
+    CAMPO_57_14:'',
+    CAMPO_58:'',
+    CAMPO_59:'',
+    CAMPO_60:'',
+    CAMPO_61:'',
+    CAMPO_62:'',
+    CAMPO_63:'',
+    CAMPO_64:'',
+    CAMPO_64_1:'',
+    CAMPO_64_2:'',
+    CAMPO_65:'',
+    CAMPO_66:'',
+    EDAD_CORTE:'',
+    EDAD_ACTUAL:'',
+    DOSIS_PROFILAXIS:'',
+  }
+  constructor(private hemofiliaservice:HemofiliaService) { }
 
   ngOnInit(): void {
+   this.Funcionesdecarga();
   }
 
+  Funcionesdecarga(){
+    this.Cargartipodocumento();
+    this.CargarSexo();
+    this.CargarRegimenafiliacion();
+    this.CargarcodigoPertenenciaetnica();
+    this.Cargargrupopoblacional();
+    this.CargarEstadogestionfechacorte();
+    this.CargarUsarioProgramaconsegeria();
+    this.Cargarmotivopruebadiagnostico();
+    this.CarTipofrecuenciaDiagnostico();
+    this.CargarClasificacionSeveridadNF();
+    this.CargarAFAsociadohemofilia();
+    this.CargarFactorrecibido();
+    this.CargarEsquema();
+    this.CargarFacorRecibidoTA();
+    this.CargarEsquemaTA();
+    this.CargarFrecuenciaSemana();
+    this.CargarModalidadapltratamiento();
+    this.CargarviaAdministración();
+    this.CargarProfesionallieratenciondelpaciente();
+    this.Cargarhermatosis();
+    this.CargarPresenciainhibidorfechacorte();
+    this.recibioiti();
+    this.CargarPacienteITI();
+    this.CargarArtropiahermofilicacronica();
+    this.CargarInfectadoporVHC();
+    this.CargarInfectadoporVHB();
+    this.CargarInfectadoporVIH();
+    this.CargarPseudotumores();
+    this.CargarFracturas();
+    this.CargarAnafilaxis();
+    this.CargarNovedad();
+    this.Cargarcausademuerte();
+  }
+
+  Cargartipodocumento(){
+    this.hemofiliaservice.CargarTipodocumento().subscribe(res=>{
+      this.tipodocumento = res;
+    })
+  }
+
+  CargarSexo(){
+    this.hemofiliaservice.CargarSexo().subscribe(res=>{
+      this.Sexo = res;
+    })
+  }
+
+  CargarRegimenafiliacion(){
+    this.hemofiliaservice.CargarRegimenAfiliacion().subscribe(res=>{
+      this.RegimenAF = res;
+    })
+  }
+
+  CargarcodigoPertenenciaetnica(){
+    this.hemofiliaservice.CargarcodigoPertenenciaetnica().subscribe(res=>{
+      this.CEtnica = res;
+    })
+  }
+  Cargargrupopoblacional(){
+    this.hemofiliaservice.Cargargrupopoblacional().subscribe(res=>{
+      this.GProblacional = res;
+    })
+  }
+
+  CargarEstadogestionfechacorte(){
+    this.hemofiliaservice.CargarEstadogestionfechacorte().subscribe(res=>{
+      this.EstagFechacorte = res;
+    })
+  }
+
+  CargarUsarioProgramaconsegeria(){
+    this.hemofiliaservice.CargarUsarioProgramaconsegeria().subscribe(res=>{
+      this.Programaconsejeria = res;
+    })
+  }
+  Cargarmotivopruebadiagnostico(){
+    this.hemofiliaservice.Cargarmotivopruebadiagnostico().subscribe(res=>{
+      this.MPDiagnostico = res;
+    })
+  }
+  CarTipofrecuenciaDiagnostico(){
+    this.hemofiliaservice.CarTipofrecuenciaDiagnostico().subscribe(res=>{
+      this.TPDiagnostico = res;
+    })
+  }
+
+  CargarClasificacionSeveridadNF(){
+    this.hemofiliaservice.CargarClasificacionSeveridadNF().subscribe(res=>{
+      this.CLSeveridad = res;
+    })
+  }
+
+  CargarAFAsociadohemofilia(){
+    this.hemofiliaservice.CargarAFAsociadohemofilia().subscribe(res=>{
+      this.ASHemofilia = res;
+    })
+  }
+
+  CargarFactorrecibido(){
+    this.hemofiliaservice.CargarFactorrecibido().subscribe(res=>{
+      this.FactorR = res;
+    })
+  }
+
+  CargarEsquema(){
+    this.hemofiliaservice.CargarEsquema().subscribe(res=>{
+      this.Esquema1 = res;
+    })
+  }
+  CargarFacorRecibidoTA(){
+    this.hemofiliaservice.CargarFacorRecibidoTA().subscribe(res=>{
+      this.FactorrecibidoTA = res;
+    })
+  }
+  CargarEsquemaTA(){
+    this.hemofiliaservice.CargarEsquemaTA().subscribe(res=>{
+      this.EsquemaTA = res;
+    })
+  }
+  CargarFrecuenciaSemana(){
+    this.hemofiliaservice.CargarFrecuenciaSemana().subscribe(res=>{
+      this.Frecuenciasemana = res;
+    })
+  }
+  CargarModalidadapltratamiento(){
+    this.hemofiliaservice.CargarModalidadapltratamiento().subscribe(res=>{
+      this.Modalidadtratamiento = res;
+    })
+  }
+  CargarviaAdministración(){
+    this.hemofiliaservice.CargarviaAdministración().subscribe(res=>{
+      this.Viaadministracion = res;
+    })
+  }
+  CargarProfesionallieratenciondelpaciente(){
+    this.hemofiliaservice.CargarProfesionallieratenciondelpaciente().subscribe(res=>{
+      this.ProfesionalSP = res;
+    })
+  }
+  Cargarhermatosis(){
+    this.hemofiliaservice.Cargarhermatosis().subscribe(res=>{
+      this.Hermatosis = res;
+    })
+  }
+  CargarPresenciainhibidorfechacorte(){
+    this.hemofiliaservice.CargarPresenciainhibidorfechacorte().subscribe(res=>{
+      this.Ingiborfechacorte = res;
+    })
+  }
+  CargarPacienteITI(){
+    this.hemofiliaservice.CargarPacienteITI().subscribe(res=>{
+      this.PacienteITI = res;
+    })
+  }
+  recibioiti(){
+    this.hemofiliaservice.recibioiti().subscribe(res=>{
+      this.RecibioITI = res;
+    })
+  }
+  CargarArtropiahermofilicacronica(){
+    this.hemofiliaservice.CargarArtropiahermofilicacronica().subscribe(res=>{
+      this.hermofiliacronica = res;
+    })
+  }
+  CargarInfectadoporVHC(){
+    this.hemofiliaservice.CargarInfectadoporVHC().subscribe(res=>{
+      this.VHC = res;
+    })
+  }
+  CargarInfectadoporVHB(){
+    this.hemofiliaservice.CargarInfectadoporVHB().subscribe(res=>{
+      this.VHB = res
+    })
+  }
+  CargarInfectadoporVIH(){
+    this.hemofiliaservice.CargarInfectadoporVIH().subscribe(res=>{
+      this.VIH = res;
+    })
+
+  }
+  CargarPseudotumores(){
+    this.hemofiliaservice.CargarPseudotumores().subscribe(res=>{
+      this.Pseudotumor = res;
+    })
+  }
+  CargarFracturas(){
+    this.hemofiliaservice.CargarFracturas().subscribe(res=>{
+      this.Fractura = res;
+    })
+  }
+  CargarAnafilaxis(){
+    this.hemofiliaservice.CargarAnafilaxis().subscribe(res=>{
+      this.Anasifilis = res;
+    })
+  }
+  CargarNovedad(){
+    this.hemofiliaservice.CargarNovedad().subscribe(res=>{
+      this.Novedad = res;
+    })
+  }
+  Cargarcausademuerte(){
+    this.hemofiliaservice.Cargarcausademuerte().subscribe(res=>{
+      this.Causademuerte = res;
+    })
+  }
 }
---
+++ PruebaCargue/src/app/services/hemofilia/hemofilia.service.ts
@@ -0,0 +1,128 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import keys from '../../../keys'
+import {Hemofilia} from '../../models/hemofilia'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HemofiliaService {
+
+  API_URI = keys.api.API_URI + '/hemofilia';
+  constructor(private http:HttpClient) { }
+
+  CargarTipodocumento(){
+    return this.http.get(`${this.API_URI}`)
+  }
+  CargarSexo(){
+    return this.http.get(`${this.API_URI}/sexo`)
+  }
+  CargarRegimenAfiliacion(){
+    return this.http.get(`${this.API_URI}/regimen`)
+  }
+  CargarcodigoPertenenciaetnica(){
+    return this.http.get(`${this.API_URI}/petnica`)
+  }
+  Cargargrupopoblacional(){
+    return this.http.get(`${this.API_URI}/gpoblacion`)
+  }
+  CargarEstadogestionfechacorte(){
+    return this.http.get(`${this.API_URI}/gestacionfechacorte`)
+  }
+  CargarUsarioProgramaconsegeria(){
+    return this.http.get(`${this.API_URI}/planificacionconsegeria`)
+  }
+  Cargarmotivopruebadiagnostico(){
+    return this.http.get(`${this.API_URI}/motivopruebadiagnostico`)
+  }
+  CarTipofrecuenciaDiagnostico(){
+    return this.http.get(`${this.API_URI}/tipofrecuenciadiagnostico`)
+  }
+  CargarClasificacionSeveridadNF(){
+    return this.http.get(`${this.API_URI}/clasificacionseveridad`)
+  }
+  CargarAFAsociadohemofilia(){
+    return this.http.get(`${this.API_URI}/afaasociadohemofilia`)
+  }
+  CargarFactorrecibido(){
+    return this.http.get(`${this.API_URI}/factorrecibido`)
+  }
+  CargarEsquema(){
+    return this.http.get(`${this.API_URI}/esquemaantecedentes`)
+  }
+  CargarFacorRecibidoTA(){
+    return this.http.get(`${this.API_URI}/factorrecibidota`)
+  }
+
+  CargarEsquemaTA(){
+    return this.http.get(`${this.API_URI}/esquemata`)
+  }
+
+  CargarFrecuenciaSemana(){
+    return this.http.get(`${this.API_URI}/frecuenciasemana`)
+  }
+
+  CargarModalidadapltratamiento(){
+    return this.http.get(`${this.API_URI}/modalidadtratamiento`)
+  }
+
+  CargarviaAdministración(){
+    return this.http.get(`${this.API_URI}/viaadministracion`)
+  }
+  CargarProfesionallieratenciondelpaciente(){
+    return this.http.get(`${this.API_URI}/profecionalliderspaciente`)
+  }
+  Cargarhermatosis(){
+    return this.http.get(`${this.API_URI}/hermatosis`)
+  }
+  CargarPresenciainhibidorfechacorte(){
+    return this.http.get(`${this.API_URI}/presenciaifechacorte`)
+  }
+  CargarPacienteITI(){
+    return this.http.get(`${this.API_URI}/pacienteiti`)
+  }
+  recibioiti(){
+    return this.http.get(`${this.API_URI}/recibioiti`)
+  }
+  CargarArtropiahermofilicacronica(){
+    return this.http.get(`${this.API_URI}/artropicronica`)
+  }
+  CargarInfectadoporVHC(){
+    return this.http.get(`${this.API_URI}/vhc`)
+  }
+  CargarInfectadoporVHB(){
+    return this.http.get(`${this.API_URI}/vhb`)
+  }
+  CargarInfectadoporVIH(){
+    return this.http.get(`${this.API_URI}/vih`)
+  }
+  CargarPseudotumores(){
+    return this.http.get(`${this.API_URI}/Pseudotumores`)
+  }
+  CargarFracturas(){
+    return this.http.get(`${this.API_URI}/fracturas`)
+  }
+  CargarAnafilaxis(){
+    return this.http.get(`${this.API_URI}/anafilaxis`)
+  }
+  CargarNovedad(){
+    return this.http.get(`${this.API_URI}/novedad`)
+  }
+  Cargarcausademuerte(){
+    return this.http.get(`${this.API_URI}/causanuerte`)
+  }
+  CargarMunicipioresidencia(NOMBRE_CIUDAD:string){
+    return this.http.get(`${this.API_URI} ${NOMBRE_CIUDAD}`,)
+  }
+  Codigovalidohabilitacionips(NOMBRE_IPS:string){
+    return this.http.get(`${this.API_URI}/codigovalidoips/${NOMBRE_IPS}`)
+  }
+  CodigoCUM35363738(CODIGO_CUP:string){
+    return this.http.get(`${this.API_URI}/cum/${CODIGO_CUP}`)
+  }
+
+  Guardarhemofilia(Hemofilia:Hemofilia){
+    return this.http.post(`${this.API_URI}/`, Hemofilia)
+  }
+
+}
