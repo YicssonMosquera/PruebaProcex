9997d7c8d270f5c70640b3a24a3c1cc880954a09
Yicsson <yibsondavid@gmail.com>
2021-05-13 16:12:07 -0500

    validacion de estructura modificada

--- backend/server/src/controller/CarguehemofiliaControllers.ts
+++ backend/server/src/controller/CarguehemofiliaControllers.ts
@@ -34,7 +34,15 @@ class Carguehemofiliacontrollers {
                             console.log('guarde campo1')
                             newDatos.CAMPO_1 = CAMPO_1
                         }
+                    } else if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15030 && result[i].VIGENTE == 'N') {
+                        if (CAMPO_1.length > result[i].LONGITUD_MAXIMA) {
+                            console.log('Longitud no corresponde campo1 ')
+                        } else {
+                            console.log('guarde campo1')
+                            newDatos.CAMPO_1 = CAMPO_1
+                        }
                     }
+
                     if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15031 && result[i].VIGENTE == 'S') {
                         if (CAMPO_2.length < result[i].LONGITUD_MINIMA || CAMPO_2.length > result[i].LONGITUD_MAXIMA) {
                             console.log('Longitud no corresponde campo2')
@@ -42,9 +50,24 @@ class Carguehemofiliacontrollers {
                             console.log('guarde campo2')
                             newDatos.CAMPO_2 = CAMPO_2
                         }
+                    } else if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15031 && result[i].VIGENTE == 'N') {
+                        if (CAMPO_2.length > result[i].LONGITUD_MAXIMA) {
+                            console.log('Longitud no corresponde campo2 ')
+                        } else {
+                            console.log('guarde campo2')
+                            newDatos.CAMPO_2 = CAMPO_2
+                        }
                     }
+
                     if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15032 && result[i].VIGENTE == 'S') {
                         if (CAMPO_3.length < result[i].LONGITUD_MINIMA || CAMPO_3.length > result[i].LONGITUD_MAXIMA) {
+                            console.log('Longitud no corresponde campo3 ')
+                        } else {
+                            console.log('guarde campo3')
+                            newDatos.CAMPO_3 = CAMPO_3
+                        }
+                    } else if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15032 && result[i].VIGENTE == 'N') {
+                        if (CAMPO_3.length > result[i].LONGITUD_MAXIMA) {
                             console.log('Longitud no corresponde campo3 ')
                         } else {
                             console.log('guarde campo3')
@@ -58,20 +81,152 @@ class Carguehemofiliacontrollers {
                             console.log('guarde campo4')
                             newDatos.CAMPO_4 = CAMPO_4
                         }
+                    } else if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15033 && result[i].VIGENTE == 'N') {
+                        if (CAMPO_1.length > result[i].LONGITUD_MAXIMA) {
+                            console.log('Longitud no corresponde campo4 ')
+                        } else {
+                            console.log('guarde campo1')
+                            newDatos.CAMPO_4 = CAMPO_4
+                        }
                     }
+
+
                     if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15034 && result[i].VIGENTE == 'S') {
                         if (CAMPO_5.length < result[i].LONGITUD_MINIMA || CAMPO_5.length > result[i].LONGITUD_MAXIMA) {
                             console.log('Longitud no corresponde campo5 ')
-                        }else if(result[i].VALORES) {
+                        } else if (result[i].VALORES) {
                             let validacion = (result[i].VALORES.split(','))
-                           if(CAMPO_5 == validacion[0] || validacion[1]== CAMPO_5  || validacion[2]== CAMPO_5  || validacion[3]== CAMPO_5  || validacion[4]== CAMPO_5  || validacion[5]== CAMPO_5  || validacion[6]== CAMPO_5  || validacion[7]== CAMPO_5  || validacion[8]== CAMPO_5  || validacion[9]== CAMPO_5   ){
-                            console.log('guarde campo5')
-                            newDatos.CAMPO_5 = CAMPO_5
-                           }else{
-                            console.log('Tipo de datos no valido campo5')
+                            if (validacion.includes(CAMPO_5)) {
+                                console.log('guarde campo5')
+                                newDatos.CAMPO_5 = CAMPO_5
+                            } else {
+                                console.log('Tipo de datos no valido campo5')
+                            }
                         }
+                    } else if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15034 && result[i].VIGENTE == 'N') {
+                        if (CAMPO_5.length > result[i].LONGITUD_MAXIMA) {
+                            console.log('Longitud no corresponde campo5 ')
+                        } else if (result[i].VALORES) {
+                            let validacion = (result[i].VALORES.split(','))
+                            if (validacion.includes(CAMPO_5) || CAMPO_5 == '') {
+                                console.log('guarde campo5')
+                                newDatos.CAMPO_5 = CAMPO_5
+                            } else {
+                                console.log('Tipo de datos no valido campo5')
+                            }
                         }
                     }
+
+                    if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15035 && result[i].VIGENTE == 'S') {
+                        if (CAMPO_6.length < result[i].LONGITUD_MINIMA || CAMPO_6.length > result[i].LONGITUD_MAXIMA) {
+                            console.log('Longitud no corresponde campo6 ')
+                        } else {
+                            console.log('guarde campo6')
+                            newDatos.CAMPO_6 = CAMPO_6
+                        }
+                    }
+
+                    if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15036 && result[i].VIGENTE == 'S') {
+                        if (CAMPO_7.length < result[i].LONGITUD_MINIMA || CAMPO_7.length > result[i].LONGITUD_MAXIMA) {
+                            console.log('Longitud no corresponde campo7 ')
+                        } else {
+                            console.log('guarde campo7')
+                            newDatos.CAMPO_7 = CAMPO_7
+                        }
+                    }
+
+                    if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15037 && result[i].VIGENTE == 'S') {
+                        if (CAMPO_8.length < result[i].LONGITUD_MINIMA || CAMPO_8.length > result[i].LONGITUD_MAXIMA) {
+                            console.log('Longitud no corresponde campo8 ')
+                        } else if (result[i].VALORES) {
+                            let validacion = (result[i].VALORES.split(','))
+                            if (CAMPO_8 == validacion[0] || validacion[1] == CAMPO_8) {
+                                console.log('guarde campo8')
+                                newDatos.CAMPO_8 = CAMPO_8
+                            } else {
+                                console.log('Tipo de datos no valido campo8')
+                            }
+                        }
+                    }
+
+                    if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15038 && result[i].VIGENTE == 'S') {
+                        if (CAMPO_9.length < result[i].LONGITUD_MINIMA || CAMPO_9.length > result[i].LONGITUD_MAXIMA) {
+                            console.log('Longitud no corresponde campo9 ')
+                        } else {
+                            console.log('guarde campo9')
+                            newDatos.CAMPO_9 = CAMPO_9
+                        }
+                    } else if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15038 && result[i].VIGENTE == 'N') {
+                        if (CAMPO_9.length > result[i].LONGITUD_MAXIMA) {
+                            console.log('Longitud no corresponde campo9 ')
+                        } else {
+                            console.log('guarde campo1')
+                            newDatos.CAMPO_9 = CAMPO_9
+                        }
+                    }
+                    if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15039 && result[i].VIGENTE == 'S') {
+                        if (CAMPO_10.length < result[i].LONGITUD_MINIMA || CAMPO_10.length > result[i].LONGITUD_MAXIMA) {
+                            console.log('Longitud no corresponde campo10 ')
+                        } else if (result[i].VALORES) {
+                            let validacion = (result[i].VALORES.split(','))
+                            if (CAMPO_10 == validacion[0] || validacion[1] == CAMPO_10 || validacion[2] == CAMPO_10 || validacion[2] == CAMPO_10 || validacion[4] == CAMPO_10) {
+                                console.log('guarde campo10')
+                                newDatos.CAMPO_10 = CAMPO_10
+                            } else {
+                                console.log('Tipo de datos no valido campo10')
+                            }
+                        }
+                    }
+
+                    if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15040 && result[i].VIGENTE == 'S') {
+                        if (CAMPO_11.length < result[i].LONGITUD_MINIMA || CAMPO_11.length > result[i].LONGITUD_MAXIMA) {
+                            console.log('Longitud no corresponde campo11 ')
+                        } else {
+                            console.log('guarde campo11')
+                            newDatos.CAMPO_11 = CAMPO_11
+                        }
+                    } else if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15040 && result[i].VIGENTE == 'N') {
+                        if (CAMPO_11.length > result[i].LONGITUD_MAXIMA) {
+                            console.log('Longitud no corresponde campo11 ')
+                        } else {
+                            console.log('guarde campo11')
+                            newDatos.CAMPO_11 = CAMPO_11
+                        }
+                    }
+
+                    if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15041 && result[i].VIGENTE == 'S') {
+                        if (CAMPO_12.length < result[i].LONGITUD_MINIMA || CAMPO_12.length > result[i].LONGITUD_MAXIMA) {
+                            console.log('Longitud no corresponde campo12 ')
+                        } else if (result[i].VALORES) {
+                            let validacion = (result[i].VALORES.split('-'))
+                            if (CAMPO_12 >= validacion[0] && CAMPO_12 <= validacion[1]) {
+                                console.log('guarde campo12')
+                                newDatos.CAMPO_12 = CAMPO_12
+                            } else {
+                                console.log('Tipo de datos no valido campo12')
+                            }
+                        }
+                    }
+
+                    if (result[i].ID_ESTRUCTURA_ARCHIVO_CAMPO == 15042 && result[i].VIGENTE == 'S') {
+                        if (CAMPO_13.length < result[i].LONGITUD_MINIMA || CAMPO_13.length > result[i].LONGITUD_MAXIMA) {
+                            console.log('Longitud no corresponde campo12 ')
+                        } else if (result[i].VALORES) {
+                            let validacion = (result[i].VALORES.split(','))
+                            if (validacion.includes(CAMPO_13)) {
+                                console.log('entre')
+                            } else {
+                                console.log('no entre')
+                            }
+                            // if (CAMPO_12  >= validacion[0] &&  CAMPO_12 <= validacion[1] ) {
+                            //     console.log('guarde campo12')
+                            //     newDatos.CAMPO_12 = CAMPO_12
+                            // } else {
+                            //     console.log('Tipo de datos no valido campo12')
+                            // }
+                        }
+                    }
+
                 }
             });
         }
